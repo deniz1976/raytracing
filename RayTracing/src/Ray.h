f4d3dc2ee66386627e3ff4e6059b94b9aad3a9e4
Deniz Mutlu <denizmine38@hotmail.com>
2025-02-26 19:21:52 +0300

    7

--- RayTracing/src/Ray.h
+++ RayTracing/src/Ray.h
@@ -6,5 +6,4 @@ struct Ray
 {
 	glm::vec3 Origin;
 	glm::vec3 Direction;
-
 };
--- RayTracing/src/WalnutApp.cpp
+++ RayTracing/src/WalnutApp.cpp
@@ -5,6 +5,11 @@
 #include "Walnut/Timer.h"
 
 #include "../src/Renderer.h"
+#include "../src/Camera.h"
+#include "Scene.h"
+
+#include <glm/gtc/type_ptr.hpp>
+
 
 using namespace Walnut;
 
@@ -12,7 +17,24 @@ class ExampleLayer : public Walnut::Layer
 {
 public:
 	ExampleLayer()
-		: m_Camera(45.0f, 0.1f, 100.0f) {}
+		: m_Camera(45.0f, 0.1f, 100.0f)
+	{
+		{
+			Sphere sphere;
+			sphere.Position = { 0.0f, 0.0f, 0.0f };
+			sphere.Radius = 0.5f;
+			sphere.Albedo = { 1.0f, 0.0f, 1.0f };
+			m_Scene.Spheres.push_back(sphere);
+		}
+
+		{
+			Sphere sphere;
+			sphere.Position = { 1.0f, 0.0f, -5.0f };
+			sphere.Radius = 1.5f;
+			sphere.Albedo = { 0.2f, 0.3f, 1.0f };
+			m_Scene.Spheres.push_back(sphere);
+		}
+	}
 
 	virtual void OnUpdate(float ts) override
 	{
@@ -26,6 +48,22 @@ public:
 		if (ImGui::Button("Render"))
 		{
 			Render();
+		}
+		ImGui::End();
+
+		ImGui::Begin("Scene");
+		for (size_t i = 0; i < m_Scene.Spheres.size(); i++)
+		{
+			ImGui::PushID(i);
+
+			Sphere& sphere = m_Scene.Spheres[i];
+			ImGui::DragFloat3("Position", glm::value_ptr(sphere.Position), 0.1f);
+			ImGui::DragFloat("Radius", &sphere.Radius, 0.1f);
+			ImGui::ColorEdit3("Albedo", glm::value_ptr(sphere.Albedo));
+
+			ImGui::Separator();
+
+			ImGui::PopID();
 		}
 		ImGui::End();
 
@@ -52,13 +90,14 @@ public:
 
 		m_Renderer.OnResize(m_ViewportWidth, m_ViewportHeight);
 		m_Camera.OnResize(m_ViewportWidth, m_ViewportHeight);
-		m_Renderer.Render(m_Camera);
+		m_Renderer.Render(m_Scene, m_Camera);
 
 		m_LastRenderTime = timer.ElapsedMillis();
 	}
 private:
 	Renderer m_Renderer;
 	Camera m_Camera;
+	Scene m_Scene;
 	uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
 
 	float m_LastRenderTime = 0.0f;
